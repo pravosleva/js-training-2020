758701eff4c397abad6348fd22e0a3b464195b86
Denis Poltoratsky <dpoltoratskiy@igatec.com>
2020-09-02 12:34:27 +0300

    master. ts

--- src/5-ts.ts
+++ src/5-ts.ts
@@ -1,6 +1,9 @@
 // yarn add -D @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser typescript
 
+import { testFn } from './5.1-importable'
+
 type TGreeting = 'hello ts'
 
 const a: TGreeting = 'hello ts'
 console.log(a)
+console.log(testFn())
